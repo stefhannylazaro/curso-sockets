99be2f5abe8fda4f3ae8f560225f6427a31c49df
Stefhanny Lazaro <stefhannylr19@gmail.com>
2020-02-03 10:51:29 -0500

    comunicacion client-server

---
+++ client/main.js
@@ -0,0 +1,17 @@
+var socket=io.connect('http://192.168.1.5:8080',{forceNew:true});
+socket.on('messages', function(data){
+    console.log(data);
+    render(data);
+});
+
+function render(data){
+    var html=data.map(function(item,index){
+        return (`
+            <div class="message">
+                <strong>${item.nickname} dice:</strong>
+                <p>${item.text}</p>
+            </div>
+        `);
+    });
+    document.getElementById('block-messages').innerHTML=html;
+}
--- server/index.js
+++ server/index.js
@@ -1,1 +1,34 @@
 console.log("hola");
+var express= require('express');
+var app=express();
+
+var server= require('http').Server(app);
+var io= require('socket.io')(server);
+
+app.use(express.static('client'));
+
+//configurar ruta
+app.get('/',function(req,res){
+    res.send("Hola mundo")
+});
+
+var messages=[{
+    id:1,
+    text:"Bienvenido al chat",
+    nickname:"luna99"
+},
+{
+    id:2,
+    text:"¿Cual es tu consulta? gustosos te atenderemos",
+    nickname:"luna99"
+}];
+
+//abrir una conexion al socket
+io.on('connection',function(socket){
+    console.log("Cliente con IP:"+socket.handshake.address+"se ha conectado");
+    socket.emit('messages',messages);
+})
+//crear un servidor
+server.listen(8080,function(){
+    console.log("server corriendooo en http://localhost:8080")
+});
